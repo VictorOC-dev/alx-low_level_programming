a9c57b9da75efb5fa7c6b4397a41c2d692ad8e45
VictorOC-dev <iyamavic9@gmail.com>
2023-09-07 14:08:10 -0700

    quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - This program makes use of fputs()
- * Description -This program prints to standard output
- * Return: 1 (Success)
+ * main - A c program that prints line of the standard error
+ * Description -  This program prints out the expected output
+ *Return: 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
