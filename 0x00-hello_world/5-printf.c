74e44c96842fca58be9827761c1b27e9c0d33905
VictorOC-dev <iyamavic9@gmail.com>
2023-09-07 09:31:05 -0700

    print c with func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line of words to display
+ * Description - This program is a test run to understand
+ * Some features of the c language
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
