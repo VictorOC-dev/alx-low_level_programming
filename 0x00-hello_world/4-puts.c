e8264c6d67bfbe4cf50fdca9354219860b424e5b
VictorOC-dev <iyamavic9@gmail.com>
2023-09-07 10:48:44 -0700

    put function for c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - This program uses put()
  * Description - This program prints out a sentence to output
  * Return: 0 (Success)
@@ -7,6 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
