c70022c51a00738b6451633df0546161726fe135
VictorOC-dev <iyamavic9@gmail.com>
2023-10-11 06:38:44 -0700

    create new dog file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,12 @@
  * @age: age of the new dog
  * @owner: the owner of the new dog
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return;
-	(*d).name = name;
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
